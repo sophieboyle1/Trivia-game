29edac627cb91ed64db2d1887d69fd658e1080d2
sophieboyle1 <sophieboylemiami@gmail.com>
2021-01-04 21:49:54 +0000

    added countdown function to timer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,14 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const scoreText = document.getElementById("score");
+const timeCount = document.getElementById("time");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
+let counter;
 
 let questions = [
     {
@@ -35,7 +37,7 @@ let questions = [
     },
 ];
 
-const CORRECT_BONUS = 10;
+const CORRECT_BONUS = 100;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
@@ -45,6 +47,8 @@ startGame = () => {
     console.log(availableQuesions);
     getNewQuestion();
 };
+
+startTimer(15);
 
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
@@ -90,7 +94,16 @@ choices.forEach((choice) => {
     });
 });
 
-incrementScore = num => {
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+    timeCount.textContent = time;
+    time--;
+    }
+}
+
+
+    incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
