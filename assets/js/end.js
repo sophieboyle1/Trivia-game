72b940a10008eee7bfb6e5064c46ddfce71765ea
sophieboyle1 <sophieboylemiami@gmail.com>
2021-01-05 12:58:25 +0000

    fixed error with local storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,7 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentScore = document.getItem('mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 5;
