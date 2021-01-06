68212a261f1f6ea959e3926270c5102c697f49b4
sophieboyle1 <sophieboylemiami@gmail.com>
2021-01-06 16:42:43 +0000

    updates to progress bar

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ const scoreText = document.getElementById("score");
 const timeCount = document.getElementById("time");
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -73,7 +73,7 @@ getNewQuestion = () => {
         return window.location.assign("end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
